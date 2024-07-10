07d57088b9142c7f6517022973f69a22466305e9
Michael Yeung <yeungmoterial123@gmail.com>
2024-07-10 22:48:56 +0800

    first commit

---
+++ beauty/beauty/doctype/beauty_currency_setting/beauty_currency_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MICHAEL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Beauty Currency Setting', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ beauty/beauty/doctype/beauty_customer_bias/test_beauty_customer_bias.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MICHAEL and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBeautyCustomerBias(FrappeTestCase):
+	pass
